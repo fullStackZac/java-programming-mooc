5cf3fa7fc469408503099b2463ec13aaa2b9029b
Zac Childress <101432054+fullStackZac@users.noreply.github.com>
2022-05-19 11:27:17 -0500

    Java MOOC Solutions

--- mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,33 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int sum = 0;
+        double average = 0.0;
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0) {
+                break;
+            }
+            
+            if(input > 0) {
+                numbers.add(input);
+            }
+        }
+        if(numbers.isEmpty()) {
+            System.out.println("Cannot calculate average");
+        }else {
+            for(int x: numbers) {
+                sum += x;
+            }
+            average = (double)sum / numbers.size();
+            System.out.println(average);
+        }
     }
 }
